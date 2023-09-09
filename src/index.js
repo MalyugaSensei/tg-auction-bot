16ddda1c33e60548915ccada434a15486ab07e8a
MalyugaSensei <loool252@mail.ru>
2023-09-09 22:07:45 +0500

    imports in main file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-const TelegramBot = require('node-telegram-bot-api')
-
-const token = '6411905843:AAGt28hKBBhLaYoUbssAGa-gYwdhW87WEjw'
-
-const bot = new TelegramBot(token, {polling: true})
+require('./commands')
+require('./db/connection')
