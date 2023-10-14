252d21f054252377d569368e9883f6b7f96ecd9d
MalyugaSensei <loool252@mail.ru>
2023-10-14 13:15:31 +0500

    Add new feature to auction form

--- auction-react-app/src/features/auction-form/ui/form.jsx
+++ auction-react-app/src/features/auction-form/ui/form.jsx
@@ -1,69 +1,153 @@
-import Form from 'react-bootstrap/Form'
-import Badge from 'react-bootstrap/Badge'
-import Button from 'react-bootstrap/Button'
 import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-const tg = window.Telegram.WebApp
+import { FormFeedback } from '@/shared/ui/FormFeedback'
+import { InputStickers } from '@/features/auction-form/ui/AddSticker'
+import { tg } from '@/shared/TelegramWebApp'
+
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import './CustomDatePicker.css'
+import './CustomTimePicker.css'
 
 const wearArray = [
-    'Factory New',
-    'Minimal Wear',
-    'Field-Tested',
-    'Well-Worm',
-    'Battle-Scarred'
+    { title: 'Factory New', value: "factory_new" },
+    { title: 'Minimal Wear', value: "minimal_wear" },
+    { title: 'Field-Tested', value: "field_-_tested" },
+    { title: 'Well-Worm', value: 'well_-_worm' },
+    { title: 'Battle-Scarred', value: 'battle_-_scarred' },
 ]
 
 export const AuctionForm = () => {
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm()
 
     useEffect(() => {
         tg.ready()
     }, [])
 
-    const onClose = () => {
-        tg.close()
+    const onSubmit = (data) => {
+        console.log(data)
+        tg.sendData(JSON.stringify(data))
     }
 
     return (
-        <Form onSubmit={onClose}>
-            <Form.FloatingLabel
-                label="Лот"
-                className='mb-3'>
-                <Form.Control type='text' placeholder='Введите наименование лота' />
-            </Form.FloatingLabel>
-            <Form.FloatingLabel
-                label="Float"
-                className='mb-3'>
-                <Form.Control type='number' step='0.01' placeholder='Введите Float лота' />
-            </Form.FloatingLabel>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className='mb-3'>
+                <Form.FloatingLabel
+                    label="Лот">
+                    <Form.Control
+                        autoComplete='off'
+                        type='text'
+                        size='lg'
+                        placeholder='Введите наименование лота'
+                        {...register("lotName", {
+                            required: 'Введите имя лота'
+                        })} />
+                </Form.FloatingLabel>
+                <FormFeedback field={errors.lotName} />
+            </Form.Group>
+            <Form.Group className='mb-3'>
+                <Form.FloatingLabel
+                    label="Float">
+                    <Form.Control
+                        autoComplete='off'
+                        type='number'
+                        step=".000000000001"
+                        size='lg'
+                        pattern="[0-9]*[.,]?[0-9]*"
+                        placeholder='Введите Float лота'
+                        {...register("lotFloat", {
+                            required: 'Введите Float лота',
+                            valueAsNumber: true,
+                            min: {
+                                value: 0,
+                                message: "Значение может быть в пределах 0 и 1"
+                            },
+                            max: {
+                                value: 1,
+                                message: "Значение может быть в пределах 0 и 1"
+                            },
+                        })} />
+                </Form.FloatingLabel>
+                <FormFeedback field={errors.lotFloat} />
+            </Form.Group>
             <Form.Group className='mb-3'>
                 <Form.Label>Изношеность</Form.Label>
                 <div className='d-flex flex-wrap'>
-                    {wearArray.map(item => (
-                        <h5>
-                            <Badge className='me-3' bg='secondary'>{item}</Badge>
-                        </h5>
-                    ))}
+                    <div role='group' className='btn-group'>
+                        {wearArray.map((item, idx) => {
+                            return (
+                                <>
+                                    <input
+                                        className="btn-check"
+                                        type="radio"
+                                        id={`radio-${idx}`}
+                                        autoComplete='off'
+                                        value={item.value}
+                                        {...register('lotWear',
+                                            { required: "Выберите изношенность предмета" }
+                                        )} />
+                                    <label tabIndex="0" htmlFor={`radio-${idx}`} className="btn btn-outline-primary btn-sm">{item.title}</label>
+                                </>
+                            )
+                        })}
+                    </div>
                 </div>
+                <FormFeedback field={errors.lotWear} />
             </Form.Group>
-
-            <Form.FloatingLabel
-                label='Наклейка'
-                className='d-flex mb-3'
-            >
-
-                <Form.Control className='me-2' type='text' placeholder='Ввдеите название наклейки' />
-                <Button variant='outline-success'>
-                    <i className='gg-check'></i>
-                </Button>
-            </Form.FloatingLabel>
-            <Form.FloatingLabel
-                label="Начальная цена"
-                className='mb-3'>
-                <Form.Control type='number' defaultValue={10} placeholder='Введите начальную цену (по умолчанию 10)' />
-            </Form.FloatingLabel>
-            <Button variant='primary' className='w-100' type='submit'>
+            <Form.Group className='mb-3'>
+                <InputStickers control={control} />
+            </Form.Group>
+            <Form.Group className='mb-3'>
+                <Form.FloatingLabel
+                    label="Начальная цена">
+                    <Form.Control
+                        autoComplete='off'
+                        type='number'
+                        size='lg'
+                        defaultValue={10}
+                        placeholder='Введите начальную цену (по умолчанию 10)'
+                        {...register("lotStartPrice", {
+                            required: "Введите начальную цену лота"
+                        })} />
+                </Form.FloatingLabel>
+            </Form.Group>
+            <Form.Group className='mb-3'>
+                <Form.FloatingLabel
+                    className='form-floating-date'
+                    label="Дата и время начала аукциона">
+                    <Form.Control
+                        type='datetime-local'
+                        size='lg'
+                        style={{ paddingTop: "1.8rem" }}
+                        placeholder='дд.мм.гггг --:--'
+                        {...register('finishAt', {
+                            required: "Обязательное поле",
+                            validate: {
+                                validateDate: (date) => {
+                                    if (new Date(date) >= new Date()) {
+                                        return true
+                                    } else {
+                                        return "Дата должна быть в будущем"
+                                    }
+                                }
+                            }
+                        })}
+                    />
+                </Form.FloatingLabel>
+                <FormFeedback field={errors.finishAt} />
+            </Form.Group>
+            <Button variant='primary' size='lg' className='w-100 mb-3' type='submit'>
                 Создать
             </Button>
-        </Form>
+        </Form >
     )
 }
