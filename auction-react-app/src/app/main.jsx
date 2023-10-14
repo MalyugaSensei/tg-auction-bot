c0bc95db246db31a40a12dc3faeb721e444f96b8
MalyugaSensei <loool252@mail.ru>
2023-10-14 13:03:31 +0500

    Formating main.jsx

--- auction-react-app/src/app/main.jsx
+++ auction-react-app/src/app/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/shared/index.css'
 const appRouter = createBrowserRouter(router)
