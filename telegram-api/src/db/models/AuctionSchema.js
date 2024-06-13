5b77c530708d35ef0a880cdca5e4125fea701665
MalyugaSensei <loool252@mail.ru>
2024-06-14 01:48:26 +0500

    Update datatypes and required fields and AutoIncrement

--- telegram-api/src/db/models/AuctionSchema.js
+++ telegram-api/src/db/models/AuctionSchema.js
@@ -1,17 +1,21 @@
 const { Schema, model } = require('mongoose')
+const { AutoIncrement } = require('../connection')
 
 const AuctionScheme = new Schema({
-    number: Number,
-    lotName: String,
-    lotFloat: Schema.Types.Decimal128,
-    lotWear: String,
+    auctionId: { type: Number, unique: true },
+    lotName: { type: String, required: true },
+    lotFloat: { type: Schema.Types.Decimal128, required: true },
+    lotWear: { type: String, required: true },
     stickers: [{
         name: String,
     }],
-    lotStartPrice: Number,
-    finishAt: Date,
+    lotStartPrice: { type: Number, required: true },
+    startAt: { type: Date, required: true },
+    finishedAt: Date,
     bidId: { type: Schema.Types.ObjectId, ref: 'Bid' },
-})
+}, { timestamps: true })
+
+AuctionScheme.plugin(AutoIncrement, { inc_field: 'auctionId', })
 
 const Auction = model('Auction', AuctionScheme)
 
